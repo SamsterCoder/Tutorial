362e8b90c09a9bd43e4a461871d8492bc6549efc
Sam Helbig <sammy.helbig@icloud.com>
2024-04-21 18:05:15 +0200

    Change: Dummyscript entfernt
    habe das Dummyscript entfernt

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -4,24 +4,14 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
-    private Rigidbody rbody;
+
+    public string[] testArray;
 
     // Start is called before the first frame update
     void Start()
     {
-        rbody = GetComponent<Rigidbody>();
-        
+
     }
 
-    private void OnMouseDown()
-    {
-        rbody.AddForce(new Vector3(0, 10, 0), ForceMode.Impulse);
-    }
 
-    public void Test ()
-    {
-        Debug.Log("Hallo!");
-    }
-    
- 
-}
+} 
